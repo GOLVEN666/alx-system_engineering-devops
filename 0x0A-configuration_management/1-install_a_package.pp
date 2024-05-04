f8feaa58f44d0de90fc42f3931120af27c9fa267
ACH <78966503+GOLVEN666@users.noreply.github.com>
2024-05-04 02:55:47 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #!/usr/bin/pup
-#   This is a Puppet script that installs the Flask Python package
+# Install an especific version of flask
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
