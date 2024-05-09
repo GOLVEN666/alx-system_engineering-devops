21fb3ec88c13d8972e493a97d4678ef7ae77400b
ACH <78966503+GOLVEN666@users.noreply.github.com>
2024-05-09 01:18:59 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# Install an especific version of flask
+# install flask from pip3.
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+
+# Ensure Werkzeug version 2.1
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
